9a036906f040e2bc091bb14dad96a4dd50e113e1
unknown <daffaputrapratamaa@gmail.com>
2023-12-09 14:18:49 +0700

    feat(auth) : Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,71 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Google, Facebook } from "@mui/icons-material";
+import { Button } from "./components/ui/button";
+import { Label } from "./components/ui/label";
+import { Input } from "./components/ui/input";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex w-full h-screen items-center justify-center">
+        <Card className="flex flex-col items-start w-full max-w-lg">
+          <CardHeader className="mb-3">
+            <CardTitle>Hi! Welcome Back!</CardTitle>
+            <CardDescription>Please enter your data to login!</CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col  w-full">
+            <div className="w-full mb-3">
+              <div className="mb-3 w-full">
+                <Label>Email</Label>
+                <Input
+                  placeholder="Input your email here.."
+                  className="w-full"
+                  type="email"
+                ></Input>
+              </div>
+              <div>
+                <div>
+                  <Label>Password</Label>
+                  <Input
+                    placeholder="Input your password here.."
+                    className="w-full inline-block"
+                    type="password"
+                  ></Input>
+                </div>
+              </div>
+            </div>
+            <Button>Login</Button>
+          </CardContent>
+
+          <p className="text-xs font-semibold text-gray-400 text-center mx-auto">
+            OR
+          </p>
+
+          <CardFooter className="flex-col gap-2 w-full pt-6">
+            <Button className="gap-2 w-full" variant={"outline"}>
+              <Google></Google>
+              Continue with Google
+            </Button>
+            <Button className="gap-2 w-full mb-2" variant={"outline"}>
+              <Facebook></Facebook>
+              Continue with Google
+            </Button>
+            <small className="text-sm font-medium leading-none">
+              Don't have account?{" "}
+              <a href="Register here!" className="font-bold underline">
+                Register here!
+              </a>
+            </small>
+          </CardFooter>
+        </Card>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
